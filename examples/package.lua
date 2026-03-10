b3f1c9bbfb34c9aa979fa8ed7bfdff4455689c68
Manuel Staufer <manuel.staufervb@gmail.com>
2026-03-10 02:48:51 +0100

    Delete examples directory

--- examples/package.lua
+++
@@ -1,9 +0,0 @@
-require ('libraries/inline')
-
-local inline = inline
-
-inline:commands('install', 'package')
-inline:commands('remove', 'package')
-inline:commands('list')
-inline:commands('publish', 'package')
-inline:commands('update', 'package', 'v0.2')
