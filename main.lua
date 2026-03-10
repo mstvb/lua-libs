d0a89c38e710b16b968c629edfeb4d6229e46264
Manuel Staufer <manuel.staufervb@gmail.com>
2026-03-10 02:48:31 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,3 @@
 --- Libraries
 require ('libraries/match') --- Import Switch-Case Statements
 require ('libraries/class') --- Import Class Implementation
-require ('libraries/inline') --- Import Package Manager
-
---- Scripts
-local inline_test = require ('examples.package') --- Test Inline Package Manager
-local game_test = require ('examples/game') --- Test Game (requires Love2D)
