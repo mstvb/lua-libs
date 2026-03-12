01076a41220c0b3965f6c4132ec18bdb94479ef6
Manuel Staufer <manuel.staufervb@gmail.com>
2026-03-12 09:36:55 +0100

    Changed to Lua Class without class.lua

--- libraries/match.lua
+++ libraries/match.lua
@@ -1,3 +1,4 @@
+Statement = {}
 --- @class statement
 --- 
 --- Attributes
@@ -12,65 +13,52 @@
 --- Returns
 --- @return table match
 ---
-function statement() 
-	local match = {}
-    match.__index = match
-	match.default = false
+function Statement:new() 
+	setmetatable({}, self)
+    self.__index = match
+	self.default = false
 
-	--- Add Match Statement
-	---
-	--- Parameters
-	--- @param key string
-	--- @param statement function
-	---
-	--- Attributes
-	--- @field match table
-	---
-	function match:add(key, statement)	
-		if key and statement then
-			self[key] = statement
-		else
-			error('Key | Statement not exists')
-		end
+--- Add Match Statement
+---
+--- Parameters
+--- @param key string
+--- @param statement function
+---
+function Statement:add(key, statement)	
+	if key and statement then
+		self[key] = statement
+	else
+		error('Key | Statement not exists')
 	end
+end
 
-	--- Set Default Option
-	---
-	--- Parameters
-	--- @param default function
-	---
-	--- Attributes
-	--- @field match table
-	---
-	function match:set_default(default)
-		if default then
-			self.default = default
-		else
-			error('Default not exists')
-		end
+--- Set Default Option
+---
+--- Parameters
+--- @param default function
+---
+function Statement:set_default(default)
+	if default then
+		self.default = default
+	else
+		error('Default not exists')
 	end
+end
 
-	--- Check if Equal Function
-	--- 
-	--- Parameters
-	--- @param equal string
-	---
-	--- Attributes
-	--- @field match table
-	--- 
-	--- Returns
-	--- @return function
-	---
-	function match:get(equal)
-		if equal then
-			return self[equal]
-		elseif type(self.default) == 'function' then
-			return self.default()
-		else
-			error('Equal not exists')
-		end
+--- Check if Equal Function
+--- 
+--- Parameters
+--- @param equal string
+--- 
+--- Returns
+--- @return function
+---
+function Statement:get(equal)
+	if equal then
+		return self[equal]
+	elseif type(self.default) == 'function' then
+		return self.default()
+	else
+		error('Equal not exists')
 	end
-
-	return match
-
 end
