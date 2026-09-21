9b19b457c74836f2f7394cd81348fa6b3555018a
Manuel Staufer <mstvb@proton.me>
2026-09-21 11:11:01 +0200

    Enhance Statement class with detailed documentation
    Updated documentation and improved error handling in the Statement class methods.

--- libraries/match.lua
+++ libraries/match.lua
@@ -1,63 +1,70 @@
-Statement = {}
---- @class statement
---- 
---- Attributes
---- @field __index table
---- @field default boolean|function
----
---- Methods
---- @field add function
---- @field set_default function
---- @field get function
---- 
---- Returns
---- @return table match
----
-function Statement:new() 
-	setmetatable({}, self)
-	self.default = false
+-- match.lua
+-- @module match
+-- @description Match-Statement-Modul für switch-case-ähnliche Logik in Lua
+-- @author mstvb/lua-libs
+-- @version 1.0.0
 
---- Add Match Statement
+local Statement = {}
+Statement.__index = Statement
+
+--- @class Statement
+--- @brief A Class for Match-Case-Logik (switch-case-Alternative)
+--- @field public __index table Metatable for Function-Access
+--- @field public default function|boolean Default-Function oder false
 ---
---- Parameters
+--- @field public add fun(key: string, statement: function): nil
+--- @field public set_default fun(default: function): nil
+--- @field public get fun(equal: string): any
+
+--- @return Statement self
+function Statement:new() 
+    local instance = setmetatable({}, self)
+    instance.default = false
+    return instance
+end
+
+--- Add Statement
 --- @param key string
 --- @param statement function
----
+--- @raise error
+--- @example
+---     stmt:add("option1", function() print("Option 1") end)
 function Statement:add(key, statement)	
-	if key and statement then
-		self[key] = statement
-	else
-		error('Key | Statement not exists')
-	end
+    if key and statement then
+        self[key] = statement
+    else
+        error('Key | Statement not exists')
+    end
 end
 
---- Set Default Option
----
---- Parameters
+--- Set Default Function
 --- @param default function
----
+--- @raise error
+--- @example
+---     stmt:set_default(function() print("Unknown Option") end)
 function Statement:set_default(default)
-	if default then
-		self.default = default
-	else
-		error('Default not exists')
-	end
+    if default then
+        self.default = default
+    else
+        error('Default not exists')
+    end
 end
 
---- Check if Equal Function
---- 
---- Parameters
+--- Call Statement
 --- @param equal string
---- 
---- Returns
---- @return function
----
+--- @return any
+--- @raise error
+--- @example
+---     stmt:get("option1")  -- Executes function when match
 function Statement:get(equal)
-	if equal then
-		return self[equal]
-	elseif type(self.default) == 'function' then
-		return self.default()
-	else
-		error('Equal not exists')
-	end
+    if equal and self[equal] then
+        return self[equal]()
+    elseif type(self.default) == 'function' then
+        return self.default()
+    else
+        error('Key "' .. tostring(equal) .. '" not found and no default exists')
+    end
 end
+
+--- @return Statement
+return Statement
