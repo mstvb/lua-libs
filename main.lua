6d30070d150ab15f6f415b6618cfd9af90492308
Manuel Staufer <mstvb@proton.me>
2026-09-21 11:13:54 +0200

    Implement demo for class and match modules
    Demonstrate usage of class and match modules with AppConfig and ActionHandler.

--- main.lua
+++ main.lua
@@ -1,3 +1,46 @@
---- Libraries
-require ('libraries/match') --- Import Switch-Case Statements
-require ('libraries/class') --- Import Class Implementation
+-- main.lua
+-- @description Demo für class + match Module
+
+local class_module = require("libraries/class")
+local match_module = require("libraries/match")
+
+--- @class AppConfig
+--- @field public greeting string
+--- @field public version string
+
+--- LOGIC
+local app = class_module:new("AppConfig")
+app:set("greeting", "Hello from mstvb/lua-libs!")
+app:set("version", "1.0.0")
+
+--- CLASS MODULE
+print("=== Class-Modul Test ===")
+print("Class Name: " .. app:__str__())
+print("Greeting: " .. app:get("greeting"))
+print("Version: " .. app:get("version"))
+
+--- MATCH MODULE
+--- @class ActionHandler
+--- @field public stmt Statement
+local handler = class_module:new("ActionHandler")
+handler.stmt = match_module:new()
+
+handler.stmt:add("start", function() 
+    print("[START] Init") 
+    return true 
+end)
+handler.stmt:add("stop", function() 
+    print("[STOP] Shutdown") 
+    return false 
+end)
+handler.stmt:set_default(function() 
+    print("[ERROR] Unknown Action") 
+    return nil 
+end)
+
+print("\n=== Match-Modul Test ===")
+handler.stmt:get("start")
+handler.stmt:get("stop")
+handler.stmt:get("unknown")
+
+print("\n=== Integration successfully ===")
